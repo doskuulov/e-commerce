a0a23957efa612e738d9e737a6a4c5e51d9deb2e
Doskulov Mirbek <iskender.doskulov@gmail.com>
2023-02-10 16:50:44 +0600

    Shop

--- app/components/screens/shop/Shop.tsx
+++ app/components/screens/shop/Shop.tsx
@@ -1,25 +1,38 @@
+import Image from 'next/image'
 import React, { FC } from 'react'
 
 import Layout from '@/layout/Layout'
+
+import Column from '@/ui/grid/Column'
+import Row from '@/ui/grid/Row'
 
 import { IProductsPage } from '@/../pages'
 
 const Shop: FC<IProductsPage> = ({ products }) => {
 	return (
 		<Layout title='Shop' description='Catalog clothing and accessories'>
-			<Row className='bg-dark-primary'>
-				<Column size={4} isPadding={false}>
-					<Image src='/images/main-4x.png' alt='' width={799} height={728} />
-				</Column>
-				<Column
-					size={5}
-					isPadding={false}
-					isCenter={false}
-					className='flex-col'
-				>
-					<HotSale />
-					<HomeInformation />
-				</Column>
+			<Row
+				className='bg-dark-primary'
+				// style={{
+				// 	backgroundBliendMode: 'multiply',
+				// 	backgroundImage: 'url(/images/texture.jpg)'
+				// }}
+			>
+				{products.map(product => (
+					<Column size={2} key={product.id}>
+						<div className='text-white'>
+							<Image
+								src={product.images[0]}
+								alt={product.name[0]}
+								width={220}
+								height={220}
+							/>
+							<h3>{product.name}</h3>
+							<div className=''>{product.price}</div>
+							<button>Add to cart</button>
+						</div>
+					</Column>
+				))}
 			</Row>
 		</Layout>
 	)
